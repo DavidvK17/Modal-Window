a64e53e5da973b267554532ddcf02f8710390f81
DavidvK17 <david.sing7@gmail.com>
2022-01-24 22:02:15 +0100

    Implementation of Modals

---
+++ README.md
@@ -0,0 +1,9 @@
+#Modal Windows
+
+This is an element which is used on most websites (eg. Cookies).
+By clicking on a specific btn, the corresponding modal is display, together with an overlay around it, blurring out the background around the modal.
+The user can close the modal by clicking esc or selecting x or clicking on the overlay.
+
+As soon as I learn bubbling and DOM traversing I will implement this functionality with those tools, to improve performance when scaling this program up.
+
+I would love to hear input from programmers whether my current solution is an efficient approach for the problem.
---
+++ script.js
@@ -0,0 +1,36 @@
+'use strict';
+
+// getting button element
+const btns = document.querySelectorAll('button');
+const modals = document.querySelectorAll('.modal');
+const overlay = document.querySelector('.overlay');
+const closers = document.querySelectorAll('span');
+
+// loop over NodeList
+// get index of each btn
+// add eventlistener to respective btn
+// loop over modals
+// when btn with specific i is clicked, corresponding modal is displayed
+
+btns.forEach((btn, i) =>
+  btn.addEventListener('click', function() {
+    modals[i].classList.add('is--active');
+    overlay.classList.add('is--active');
+
+    // Close modal onkeydown esc
+    document.onkeydown = e => {
+      if (e.key === 'Escape') closeModal();
+    };
+
+    // close on clicking x
+    closers.forEach(closer => closer.addEventListener('click', closeModal));
+
+    // close on clicking on overlay
+    overlay.addEventListener('click', closeModal);
+
+    function closeModal() {
+      modals[i].classList.remove('is--active');
+      overlay.classList.remove('is--active');
+    }
+  })
+);
